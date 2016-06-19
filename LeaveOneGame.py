d35761a5099979d5e3e46cc0b8fbce89436c87bf
McLovin <mclovin@localhost.localdomain>
2016-06-19 14:34:29 -0400

    Saving work.

--- LeaveOneGame.py
+++ LeaveOneGame.py
@@ -15,40 +15,57 @@ def generateRandomMove(AdjList, NodeState):
     for node in NodeState:
         if NodeState[node] == 1:
             openNodes.append(node)
-    print openNodes
+    #print openNodes
 
     #Determine if which open nodes could be a move
     for node in openNodes:
         routes = AdjList[node]
-        print str(node) + " : " + str(routes)
+        #print str(node) + " : " + str(routes)
         for route in routes:
-            print route
+            #print route
             if NodeState[route[0]] == 0 and NodeState[route[1]] == 0:
                 move = route[0] + node + route[1]
-                print str("MOVE(") + str(move) + "): " + str(route[0]) + " -> " + str(node) + " remove " + str(route[1])
+                #print str("MOVE(") + str(move) + "): " + str(route[0]) + " -> " + str(node) + " remove " + str(route[1])
                 moves.append(move)
     return generateRandomNode(moves)
-    return 0
 
 def generateRandomNode(Nodes):
+    if len(Nodes) == 0:
+        return ""
     r = randint(0,len(Nodes)-1)
     return Nodes[r]
 
 def playGame(AdjList):
     #Setup game
+    moves = []
     nodeState = dict()
     nodes = []
     for node in adjList:
         nodes.append(node)
         nodeState[node] = 0
 
-    #Randomly pick a node to open to start the game
+    # Randomly pick a node to open to start the game
     nodeState[generateRandomNode(nodes)] = 1
     print nodeState
 
-    #Randomly select an available move
-    move = generateRandomMove(AdjList, nodeState)
-    print "MOVE: " + str(move)
+    # Randomly select an available move
+    while(True):
+        move = generateRandomMove(AdjList, nodeState)
+        if not move:
+            break
+        moves.append(move)
+        #print "MOVE: " + str(move)
+        print str("MOVE(") + str(move) + "): " + str(move[0]) + " -> " + str(move[1]) + " remove " + str(move[2])
+        nodeState[move[0]] = 1
+        nodeState[move[1]] = 0
+        nodeState[move[2]] = 1
+    pegs = len(AdjList)
+    for key in nodeState:
+        pegs = pegs - nodeState[key]
+    #print nodeState
+    #print "Pegs left: " + str(pegs)
+    print moves
+    return pegs
 
 
 adjList = {'A': [["D", "B"],["F", "C"]],
@@ -67,6 +84,12 @@ adjList = {'A': [["D", "B"],["F", "C"]],
            'N': [["L", "M"], ["E", "I"]],
            'O': [["M", "N"], ["F", "J"]]}
 
-playGame(adjList)
+#print playGame(adjList)
+
+while(True):
+    pegs = playGame(adjList)
+    print pegs
+    if pegs == 1:
+        break
 
 

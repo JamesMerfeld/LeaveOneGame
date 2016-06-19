a34964fa35a960d8f5ab04790dc51ada334f6a4b
McLovin <mclovin@localhost.localdomain>
2016-06-19 13:28:40 -0400

    Saving work.

--- LeaveOneGame.py
+++ LeaveOneGame.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def printAdjList(AdjList):
     out = ""
     for key in AdjList:
@@ -5,6 +7,49 @@ def printAdjList(AdjList):
         for route in adjList[key]:
             out += str(route[0]) + "(" + str(route[1]) + ") "
         print out
+
+def generateRandomMove(AdjList, NodeState):
+    moves = []
+    #Establish list of open nodes
+    openNodes = []
+    for node in NodeState:
+        if NodeState[node] == 1:
+            openNodes.append(node)
+    print openNodes
+
+    #Determine if which open nodes could be a move
+    for node in openNodes:
+        routes = AdjList[node]
+        print str(node) + " : " + str(routes)
+        for route in routes:
+            print route
+            if NodeState[route[0]] == 0 and NodeState[route[1]] == 0:
+                move = route[0] + node + route[1]
+                print str("MOVE(") + str(move) + "): " + str(route[0]) + " -> " + str(node) + " remove " + str(route[1])
+                moves.append(move)
+    return generateRandomNode(moves)
+    return 0
+
+def generateRandomNode(Nodes):
+    r = randint(0,len(Nodes)-1)
+    return Nodes[r]
+
+def playGame(AdjList):
+    #Setup game
+    nodeState = dict()
+    nodes = []
+    for node in adjList:
+        nodes.append(node)
+        nodeState[node] = 0
+
+    #Randomly pick a node to open to start the game
+    nodeState[generateRandomNode(nodes)] = 1
+    print nodeState
+
+    #Randomly select an available move
+    move = generateRandomMove(AdjList, nodeState)
+    print "MOVE: " + str(move)
+
 
 adjList = {'A': [["D", "B"],["F", "C"]],
            'B': [["G", "D"],["I", "E"]],
@@ -22,9 +67,6 @@ adjList = {'A': [["D", "B"],["F", "C"]],
            'N': [["L", "M"], ["E", "I"]],
            'O': [["M", "N"], ["F", "J"]]}
 
-#Generate a list of possible nodes
-nodes = []
-for node in adjList:
-    nodes.append(node)
+playGame(adjList)
 
-printAdjList(adjList)
+
